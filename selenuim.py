53ce2a1542c77706fa58fc7026cef3d5ce982ca2
Chaima Bouchareb <chaimabouchareb17@gmail.com>
2023-03-05 02:59:41 +0100

    auto search for scrapy tuto

---
+++ selenuim.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+import time  
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+PATH="C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.get("https://www.google.com/")
+search = "scrapy tuto"
+time.sleep(1)
+mysearch = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
+mysearch.send_keys(search)
+btn = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]').click()
+time.sleep(5)
